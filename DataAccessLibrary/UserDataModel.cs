206453a0e828a85a996988f8ab04d27f10c24e06
ersyn_sultanbaev <ersyn_sultanbaev@mail.ru>
2019-12-19 01:05:50 +0600

    Добавьте файлы проекта.

---
+++ DataAccessLibrary/UserDataModel.cs
@@ -0,0 +1,14 @@
+﻿using ModelsLibrary;
+using System.Data.Entity;
+
+namespace DataAccessLibrary
+{
+    public partial class UserDataModel : DbContext
+    {
+        public UserDataModel()
+            : base("name=UserDataModel")
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ IoCNinject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IoCNinject.MvcApplication" Language="C#" %>
